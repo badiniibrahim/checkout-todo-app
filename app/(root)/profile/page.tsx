8cd989f5882eafb8e311ac7963a3adf849bf5d9e
Badini Ibrahim <sawadogo.badiniibrahim@gmail.com>
2024-02-23 11:10:26 +0100

    add stripe webhooks

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { auth } from "@clerk/nextjs";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 
-const Profile = () => {
-  return <div>Profile</div>;
+import Header from "@/components/shared/Header";
+import { getUserById } from "@/lib/actions/user.actions";
+
+const Profile = async () => {
+  const { userId } = auth();
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
+
+  return (
+    <>
+      <Header title="Profile" />
+
+      <section className="profile">
+        <div className="profile-balance">
+          <p className="p-14-medium md:p-16-medium">CREDITS AVAILABLE</p>
+          <div className="mt-4 flex items-center gap-4">
+            <Image
+              src="/assets/icons/coins.svg"
+              alt="coins"
+              width={50}
+              height={50}
+              className="size-9 md:size-12"
+            />
+            <h2 className="h2-bold text-dark-600">{user.creditBalance}</h2>
+          </div>
+        </div>
+      </section>
+
+      <section className="mt-8 md:mt-14"></section>
+    </>
+  );
 };
 
 export default Profile;
